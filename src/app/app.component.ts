64941596c3ca741bd14e55d381f3a5f66050fc2e
ProjetoMensal <fvcominetti@gmail.com>
2024-10-15 19:08:47 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScrollbarComponent } from "./components/scrollbar/scrollbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScrollbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
